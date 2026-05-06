602e1b3644df9af44cbaaae37e8600204b9d9e96
Kajal Kumari <kajal345rajput@gmail.com>
2026-05-06 13:40:30 +0530

    fix login route

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import login_view, home_view, logout_view
 
 urlpatterns = [
-    path('', login_view, name='login'),        # http://127.0.0.1:8000/
-    path('home/', home_view, name='home'),     # dashboard after login
+    path('', login_view, name='login'),          # local works
+    path('login/', login_view, name='login'),    #  FIX for Render
+
+    path('home/', home_view, name='home'),
     path('logout/', logout_view, name='logout'),
 ]
